25566307f2af290c8878f02e60d9c65dabaf665f
Revillla <lachlanreville4575@gmail.com>
2020-06-14 20:15:58 +1000

    Adding Header Tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
+import Head from "next/head"
 import './styles.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Receptionist</title>
+                <link rel="icon" type="image/png" href="https://receptioni.st/img/Logo-3.png" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
