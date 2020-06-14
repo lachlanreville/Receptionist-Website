167a2923377d3630fde854382cc789a93d71fd1b
Revillla <lachlanreville4575@gmail.com>
2020-06-14 19:50:54 +1000

    Changed Discord oAuth Redirect

--- pages/api/login/index.js
+++ pages/api/login/index.js
@@ -1,6 +1,6 @@
 export default (req, res) => {
     res.writeHead(301, {
-        Location: "https://discord.com/api/oauth2/authorize?client_id=697932571601797130&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Flogin%2Fredirect&response_type=code&scope=identify%20guilds"
+        Location: "https://discord.com/api/oauth2/authorize?client_id=697932571601797130&redirect_uri=https%3A%2F%2Freceptioni.st%2Fapi%2Flogin%2Fredirect&response_type=code&scope=guilds%20identify"
         //add other headers here...
     });
     res.end();
