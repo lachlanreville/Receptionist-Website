0b591f2e7412edc6dbb9b9eaa3432a188ac0e967
revillla <lachlanreville4575@gmail.com>
2020-08-31 20:56:25 +1000

    giving guildData

--- pages/api/guilds/[guild]/index.js
+++ pages/api/guilds/[guild]/index.js
@@ -3,8 +3,24 @@ import axios from "axios"
 export default async (req, res) => {
     const { guild } = req.query
     const { access, refresh } = req.body;
+    let data
+    try {
+        data = await axios.post("https://receptioni.st/api/guilds/", { access, refresh })
+    }
+    catch (err) {
+        if (err.response.status == 401) {
+            res.statusCode = 401;
+            res.json({ success: false, message: "401 Unauthorized Access" })
+            res.end();
+        }
+        if (err.response.status == 429) {
+            res.statusCode = 429;
+            res.json({ success: false, message: "429 Rate Limited" })
+            res.end();
+        }
+    }
 
-    let { data } = await axios.post("https://receptioni.st/api/guilds/", { access, refresh })
+    data = data.data;
 
     let serverData = data.filter(c => { return c.id == guild })
 
@@ -14,6 +30,6 @@ export default async (req, res) => {
         res.end();
     }
 
-    res.json({ success: true })
+    res.json({ success: true, guildData: data })
     res.end()
 }
