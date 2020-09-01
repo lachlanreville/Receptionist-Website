74f5715e6c1cadbad81f3e2ad12ea35f386fc013
revillla <lachlanreville4575@gmail.com>
2020-09-01 16:42:39 +1000

    remove log

--- pages/api/guilds/[guild]/index.js
+++ pages/api/guilds/[guild]/index.js
@@ -45,7 +45,6 @@ export default async (req, res) => {
     catch (err) {
         console.log(err)
     }
-    console.log(guildDatabase)
 
     res.json({ success: true, guildData: serverData[0], discordData: guildInfo, guildDatabase: guildDatabase.data })
     res.end()
