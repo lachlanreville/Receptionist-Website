ab1492ac5b02c4306587dfd902849505291a37e4
revillla <lachlanreville4575@gmail.com>
2020-09-13 18:27:50 +1000

    fuck u vercel

--- pages/dashboard/[guild]/applications/index.jsx
+++ pages/dashboard/[guild]/applications/index.jsx
@@ -71,6 +71,7 @@ export default function Home() {
 
     return (
         <>
+            <div></div>
             <DisplayData guildData={serverData}>
                 <Break height="50" />
                 <Row>
