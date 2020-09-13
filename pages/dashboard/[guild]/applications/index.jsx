483be00e66b9e8f869b99f2d46bb53493955f2f1
revillla <lachlanreville4575@gmail.com>
2020-09-13 13:43:37 +1000

    fix

--- pages/dashboard/[guild]/applications/index.jsx
+++ pages/dashboard/[guild]/applications/index.jsx
@@ -49,7 +49,7 @@ export default function Home() {
         <>
             <DisplayData guildData={serverData}>
                 <Break height="100" />
-                {applications ? applications.map((application, position) => <DisplayApplications application={application} position={position} />) : <img src="https://receptioni.st/img/ReceptionistLoadingScreen.gif" alt="Loading Gif" width="256" height="256" style={{ margin: "auto" }} />
+                {applications ? applications.map((application, position) => <DisplayApplications guild={guild} application={application} position={position} />) : <img src="https://receptioni.st/img/ReceptionistLoadingScreen.gif" alt="Loading Gif" width="256" height="256" style={{ margin: "auto" }} />
                 }
 
             </DisplayData>
@@ -70,7 +70,7 @@ const DisplayApplications = (props) => {
     console.log(props)
     return (
         <div key={props.position}>
-            <a href={"https://receptioni.st/dashboard/" + guild + "/applications/" + props.application.applicationId}>{props.application.applicationName}</a>
+            <a href={"https://receptioni.st/dashboard/" + props.guild + "/applications/" + props.application.applicationId}>{props.application.applicationName}</a>
         </div>
     )
 }
