d10505c2a97f633378fd4c205ca14a46e8d421f0
revillla <lachlanreville4575@gmail.com>
2020-09-13 18:58:59 +1000

    test

--- pages/dashboard/[guild]/applications/index.jsx
+++ pages/dashboard/[guild]/applications/index.jsx
@@ -56,18 +56,15 @@ export default function Home() {
     const DisplayApplications = (props) => {
         return (
             <Column size="5" key={props.position}>
-                <a className={styles.applicationName} href="/" onClick={() => router.push(router.pathname + `?application=${props.application.applicationId}`, `/dashboard/${guild}/applications?application=${props.application.applicationId}`, { shallow: true })} > {props.application.applicationName}</a>
+                <a className={styles.applicationName} href="#" onClick={() => router.push(router.pathname + `?application=${props.application.applicationId}`, `/dashboard/${guild}/applications?application=${props.application.applicationId}`, { shallow: true })} > {props.application.applicationName}</a>
             </Column>
         )
     }
 
     useEffect(() => {
-        console.log(newApp)
-        if (!newApp) return;
         console.log(router)
-        router.push(`?application=${newApp}`, undefined, { shallow: true })
 
-    }, [newApp])
+    }, [router])
 
     return (
         <>
